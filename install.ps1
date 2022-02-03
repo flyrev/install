3322dca443bd0d8468dde6066037c3e06071b8a1
Christian Neverdal <flyrev@gmail.com>
2022-02-03 17:08:44 +0000

    Create install.ps1
    We have to start somewhere. This is a random list of things one could install.

---
+++ install.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco feature enable -n allowGlobalConfirmation
+choco upgrade all
+choco install googlechrome
+choco install discord
+choco install 7zip.install
+choco install mingw
+choco install make
+choco install cmake
+choco install powertoys
+choco install pdftk
+choco install yarn
